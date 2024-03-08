d5a3d4d78eecd9b53e5203a07893d12eec58e733
topi123098 <159298939+topi123098@users.noreply.github.com>
2024-03-08 22:17:31 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+(function () {
+  /************ animation on scroll js start ********************/
+  "use strict";
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+
+  /******************* animation on scroll js end *******************/
+
+  /********  hero typing effects adding start *********/
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+
+  /********  hero typing effects adding end *********/
+
+})(
+
+
+  
+);
