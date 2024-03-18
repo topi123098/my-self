5364866b2b98e5f5b6063c660c0342f99a00bc7b
topi123098 <159298939+topi123098@users.noreply.github.com>
2024-03-18 21:57:31 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,43 @@
 (function () {
-  /************ animation on scroll js start ********************/
   "use strict";
+
+  /*******easy selector helper function *******/
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /*******easy event listener function *******/
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  }
+
+  /***************** SHOW MENU ON SCROLL ******************/
+  
+  /*********** mobile nav-bar toggle start **********/
+
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("body").classList.toggle("mobile-nav-active");
+    // e.preventDefault();
+    this.classList.toggle("bi-menu-button-wide-fill");
+    this.classList.toggle("bi-x");
+  });
+  /*********** mobile nav-bar toggle end **********/
+
+  /************ animation on scroll js start ********************/
   window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
@@ -20,24 +57,12 @@
     new Typed(".typed", {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
+      typeSpeed: 200,
       backSpeed: 50,
       backDelay: 2000,
     });
   }
+  /********  hero typing effects adding end
 
   /********  hero typing effects adding end *********/
-
-  /*********** mobile nav-bar toggle **********/
-
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("body").classList.toggle("mobile-nav-active");
-    // e.preventDefault();
-    this.classList.toggle("bi-menu-button-wide-fill");
-    this.classList.toggle("bi-x");
-  });
-
-
-
-  
 })();
